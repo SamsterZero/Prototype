7c92fad6d84ee78b21f6451b30894cd56e50715d
SamsterZero <90505086+SamsterZero@users.noreply.github.com>
2021-12-12 19:19:19 +0530

    Add files via upload

---
+++ Login or Sign Up.php
@@ -0,0 +1,11 @@
+<?php
+	echo $_POST['email']
+	echo $_POST['username']
+	if(isset($_POST['userpass1'] === $_POST['userpass2'])){
+		userpass== $_POST['userpass1']
+		echo userpass
+	}
+	else{
+		echo "re-enter Password"
+	}
+?>
---
+++ Prototype.js
@@ -0,0 +1,43 @@
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+ }
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slidePosition-1].style.display = "block";
+ }
+
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
+  slides[slidePosition-1].style.display = "block";
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+}
+
+function changeMode() {
+  var mybody = document.body;
+  mybody.classList.toggle("mydark");
+}
